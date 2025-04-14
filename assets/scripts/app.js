8c09be4822977acef81a729fd50fb4e493fe9fc3
KeizerGR <KeizerGR@proton.me>
2025-04-14 17:01:54 +0200

    impulsloket 7mei

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -30,235 +30,235 @@ const participants = [
         ],
         "calendar_summary": "Op maandagen van 14:00 tot 16:00 uur en woensdagen van 10:00 tot 12:00 uur."
     },
-    // // {
-    // //     "name": "Spreekuur web in de wijk",
-    // //     "logo": {
-    // //         "filename": "logo_webwijk.png",
-    // //         "alt": "Web in de wijk"
-    // //     },
-    // //     "description": "Voor vragen over subsidies, uitkeringen, aanvragen DigiD, formulieren overheid, woningstichting en veel meer.",
-    // //     "calendar": [
-    // //         {
-    // //             "day": "monday",
-    // //             "start": "13:00",
-    // //             "end": "14:30",
-    // //             "frequency": "weekly"
-    // //         }
-    // //     ],
-    // //     "calendar_summary": "Op maandagen van 13:00 tot 14.30 uur."
-    // // },
-    // {
-    //     "name": "Informatieloket digitale overheid",
-    //     "logo": {
-    //         "filename": "logo_bibliotheekveendam.png",
-    //         "alt": "Bibliotheek Veendam"
-    //     },
-    //     "description": "Hulp bij contact met de digitale overheid en digitale hulp algemeen.",
-    //     "calendar": [
-    //         {
-    //             "day": "monday",
-    //             "start": "14:00",
-    //             "end": "16:00",
-    //             "frequency": "weekly"
-    //         },
-    //         {
-    //             "day": "wednesday",
-    //             "start": "10:00",
-    //             "end": "12:00",
-    //             "frequency": "weekly"
-    //         }
+    {
+        "name": "Spreekuur web in de wijk",
+        "logo": {
+            "filename": "logo_webwijk.png",
+            "alt": "Web in de wijk"
+        },
+        "description": "Voor vragen over subsidies, uitkeringen, aanvragen DigiD, formulieren overheid, woningstichting en veel meer.",
+        "calendar": [
+            {
+                "day": "monday",
+                "start": "13:00",
+                "end": "14:30",
+                "frequency": "weekly"
+            }
+        ],
+        "calendar_summary": "Op maandagen van 13:00 tot 14.30 uur."
+    },
+    {
+        "name": "Informatieloket digitale overheid",
+        "logo": {
+            "filename": "logo_bibliotheekveendam.png",
+            "alt": "Bibliotheek Veendam"
+        },
+        "description": "Hulp bij contact met de digitale overheid en digitale hulp algemeen.",
+        "calendar": [
+            {
+                "day": "monday",
+                "start": "14:00",
+                "end": "16:00",
+                "frequency": "weekly"
+            },
+            {
+                "day": "wednesday",
+                "start": "10:00",
+                "end": "12:00",
+                "frequency": "weekly"
+            }
 
-    //     ],
-    //     "calendar_summary": "Op maandagen van 14:00 tot 16:00 uur en woensdagen van 10.00 tot 12.00 uur."
-    // },
-    // {
-    //     "name": "Loket informatie geheugenproblemen",
-    //     "logo": {
-    //         "filename": "logo_loketgeheugen.png",
-    //         "alt": "Buurtzorg, Zorggroep Meander, De Basis"
-    //     },
-    //     "description": "",
-    //     "calendar": [
-    //         {
-    //             "day": "monday",
-    //             "start": "14:00",
-    //             "end": "16:00",
-    //             "frequency": "weekly"
-    //         }
-    //     ],
-    //     "calendar_summary": "Op maandagen van 14:00 tot 16:00 uur."
-    // },
-    // {
-    //     "name": "Loket Leefbaarheid",
-    //     "logo": {
-    //         "filename": "logo_loketleefbaarheid.png",
-    //         "alt": "Groninger Dorpen, Loko Loket, Loket Leefbaarheid"
-    //     },
-    //     "description": "Heb je samen met buurtgenoten een leuk plan bedacht dat bijdraagt aan prettig wonen in jouw straat of dorp? Loket Leefbaarheid is een subsidieregeling voor inwoners uit de hele provincie Groningen.",
-    //     "calendar": [
-    //         {
-    //             "day": "wednesday",
-    //             "start": "10:00",
-    //             "end": "12:00",
-    //             "frequency": "odd_weeks"
-    //         }
-    //     ],
-    //     "calendar_summary": "Oneven weken op woensdag van 10:00 tot 12:00 uur."
-    // },
-    // {
-    //     "name": "Loket Buurtgezinnen",
-    //     "logo": {
-    //         "filename": "logo_loketbuurtgezinnen.png",
-    //         "alt": "Buurtgezinnen"
-    //     },
-    //     "description": "Onder het motto 'Opvoeden doen we samen', koppelt Buurtgezinnen gezinnen die steun kunnen gebruiken aan een stabiel gezin in de buurt. Zo krijgen kinderen wat extra liefde en aandacht en worden ouders ontlast.",
-    //     "calendar": [
-    //         {
-    //             "day": "wednesday",
-    //             "start": "10:00",
-    //             "end": "12:00",
-    //             "frequency": "odd_weeks"
-    //         }
-    //     ],
-    //     "calendar_summary": "Oneven weken op woensdag van 10:00 tot 12:00 uur."
-    // },
-    // {
-    //     "name": "Informatiepunt Taalhuis",
-    //     "logo": {
-    //         "filename": "logo_bibliotheekveendam.png",
-    //         "alt": "Bibliotheek Veendam"
-    //     },
-    //     "description": "Tijdens dit spreekuur kun je samen met de Taalhuiscoördinator bekijken wat je nodig hebt en wat bij je past. Dat kan bijvoorbeeld een oefenboek zijn of een cursus.",
-    //     "calendar": [
-    //         {
-    //             "day": "monday",
-    //             "start": "14:00",
-    //             "end": "15:00",
-    //             "frequency": "weekly"
-    //         }
-    //     ],
-    //     "calendar_summary": "Op maandagen van 14:00 tot 15:00 uur."
-    // },
-    // {
-    //     "name": "Hulp bij woningzaken",
-    //     "logo": {
-    //         "filename": "logo_acantus.png",
-    //         "alt": "Acantus"
-    //     },
-    //     "description": "Voor vragen over woning huren, doorschuiven naar een ander huis, leefbaarheid in je wijk en meer.",
-    //     "calendar": [
-    //         {
-    //             "day": "monday",
-    //             "start": "14:00",
-    //             "end": "16:00",
-    //             "frequency": "weekly"
-    //         }
-    //     ],
-    //     "calendar_summary": "Op maandagen van 14:00 tot 16:00 uur."
-    // },
-    // {
-    //     "name": "Maatje voor hulp bij geldzaken",
-    //     "logo": {
-    //         "filename": "logo_schuldhm.png",
-    //         "alt": "SchuldHulpMaatje"
-    //     },
-    //     "description": "Voor vragen over hulp en/of een maatje bij geldzaken.",
-    //     "calendar": [
-    //         {
-    //             "day": "monday",
-    //             "start": "14:00",
-    //             "end": "16:00",
-    //             "frequency": "even_weeks"
-    //         },
-    //         {
-    //             "day": "wednesday",
-    //             "start": "10:00",
-    //             "end": "12:00",
-    //             "frequency": "odd_weeks"
-    //         }
-    //     ],
-    //     "calendar_summary": "Even weken op maandag van 14:00 tot 16:00 uur, oneven weken op woensdag van 10:00 tot 12:00 uur."
-    // },
-    // {
-    //     "name": "Inloopspreekuur FNV",
-    //     "logo": {
-    //         "filename": "logo_fnv.png",
-    //         "alt": "FNV"
-    //     },
-    //     "description": "Voor vragen over je loopbaan of voor een gesprek over je werk met een coach.",
-    //     "calendar": [
-    //         {
-    //             "day": "wednesday",
-    //             "start": "10:00",
-    //             "end": "12:00",
-    //             "frequency": "even_weeks"
-    //         }
-    //     ],
-    //     "calendar_summary": "Even weken op woensdag van 10:00 tot 12:00 uur."
-    // },
-    // {
-    //     "name": "Inloopspreekuur VKB",
-    //     "logo": {
-    //         "filename": "Logo_vkb.png",
-    //         "alt": "VKB"
-    //     },
-    //     "description": "De Volkskredietbank Noord Oost Groningen biedt hulp bij schulden en financiële vragen namens en in de gemeenten Veendam, Eemsdelta, Het Hogeland, Oldambt of Pekela.",
-    //     "calendar": [
-    //         {
-    //             "day": "monday",
-    //             "start": "13:00",
-    //             "end": "15:00",
-    //             "frequency": "weekly"
-    //         }
-    //     ],
-    //     "calendar_summary": "Wekelijks op maandag van 13:00 tot 15:00 uur."
-    // },
-    // {
-    //     "name": "Vrijwilligers informatiepunt",
-    //     "logo": {
-    //         "filename": "logo_vip.png",
-    //         "alt": "Vrijwilligers Informatiepunt"
-    //     },
-    //     "description": "Kom langs bij ons voor het zoeken of plaatsen van vacatures, voor informatie, advies en belangenbehartiging en deskundigheidsbevordering van vrijwilligers.",
-    //     "calendar": [
-    //         {
-    //             "day": "monday",
-    //             "start": "14:00",
-    //             "end": "16:00",
-    //             "frequency": "first_of_month"
-    //         },
-    //         {
-    //             "day": "monday",
-    //             "start": "14:00",
-    //             "end": "16:00",
-    //             "frequency": "third_of_month"
-    //         }
-    //     ],
-    //     "calendar_summary": "Iedere eerste en derde maandag van de maand van 13:00 tot 15:00 uur."
-    // },
-    // {
-    //     "name": "Loket Stichting Leergeld",
-    //     "logo": {
-    //         "filename": "logo_stleergeld.png",
-    //         "alt": "Stichting Leergeld"
-    //     },
-    //     "description": "Je kunt een vergoeding aanvragen voor activiteiten op het gebied van onderwijs, sport, cultuur en welzijn.",
-    //     "calendar": [
-    //         {
-    //             "day": "monday",
-    //             "start": "14:00",
-    //             "end": "16:00",
-    //             "frequency": "first_of_month"
-    //         },
-    //         {
-    //             "day": "monday",
-    //             "start": "14:00",
-    //             "end": "16:00",
-    //             "frequency": "third_of_month"
-    //         }
-    //     ],
-    //     "calendar_summary": "Iedere eerste en derde maandag van de maand van 13:00 tot 15:00 uur."
-    // },
+        ],
+        "calendar_summary": "Op maandagen van 14:00 tot 16:00 uur en woensdagen van 10.00 tot 12.00 uur."
+    },
+    {
+        "name": "Loket informatie geheugenproblemen",
+        "logo": {
+            "filename": "logo_loketgeheugen.png",
+            "alt": "Buurtzorg, Zorggroep Meander, De Basis"
+        },
+        "description": "",
+        "calendar": [
+            {
+                "day": "monday",
+                "start": "14:00",
+                "end": "16:00",
+                "frequency": "weekly"
+            }
+        ],
+        "calendar_summary": "Op maandagen van 14:00 tot 16:00 uur."
+    },
+    {
+        "name": "Loket Leefbaarheid",
+        "logo": {
+            "filename": "logo_loketleefbaarheid.png",
+            "alt": "Groninger Dorpen, Loko Loket, Loket Leefbaarheid"
+        },
+        "description": "Heb je samen met buurtgenoten een leuk plan bedacht dat bijdraagt aan prettig wonen in jouw straat of dorp? Loket Leefbaarheid is een subsidieregeling voor inwoners uit de hele provincie Groningen.",
+        "calendar": [
+            {
+                "day": "wednesday",
+                "start": "10:00",
+                "end": "12:00",
+                "frequency": "odd_weeks"
+            }
+        ],
+        "calendar_summary": "Oneven weken op woensdag van 10:00 tot 12:00 uur."
+    },
+    {
+        "name": "Loket Buurtgezinnen",
+        "logo": {
+            "filename": "logo_loketbuurtgezinnen.png",
+            "alt": "Buurtgezinnen"
+        },
+        "description": "Onder het motto 'Opvoeden doen we samen', koppelt Buurtgezinnen gezinnen die steun kunnen gebruiken aan een stabiel gezin in de buurt. Zo krijgen kinderen wat extra liefde en aandacht en worden ouders ontlast.",
+        "calendar": [
+            {
+                "day": "wednesday",
+                "start": "10:00",
+                "end": "12:00",
+                "frequency": "odd_weeks"
+            }
+        ],
+        "calendar_summary": "Oneven weken op woensdag van 10:00 tot 12:00 uur."
+    },
+    {
+        "name": "Informatiepunt Taalhuis",
+        "logo": {
+            "filename": "logo_bibliotheekveendam.png",
+            "alt": "Bibliotheek Veendam"
+        },
+        "description": "Tijdens dit spreekuur kun je samen met de Taalhuiscoördinator bekijken wat je nodig hebt en wat bij je past. Dat kan bijvoorbeeld een oefenboek zijn of een cursus.",
+        "calendar": [
+            {
+                "day": "monday",
+                "start": "14:00",
+                "end": "15:00",
+                "frequency": "weekly"
+            }
+        ],
+        "calendar_summary": "Op maandagen van 14:00 tot 15:00 uur."
+    },
+    {
+        "name": "Hulp bij woningzaken",
+        "logo": {
+            "filename": "logo_acantus.png",
+            "alt": "Acantus"
+        },
+        "description": "Voor vragen over woning huren, doorschuiven naar een ander huis, leefbaarheid in je wijk en meer.",
+        "calendar": [
+            {
+                "day": "monday",
+                "start": "14:00",
+                "end": "16:00",
+                "frequency": "weekly"
+            }
+        ],
+        "calendar_summary": "Op maandagen van 14:00 tot 16:00 uur."
+    },
+    {
+        "name": "Maatje voor hulp bij geldzaken",
+        "logo": {
+            "filename": "logo_schuldhm.png",
+            "alt": "SchuldHulpMaatje"
+        },
+        "description": "Voor vragen over hulp en/of een maatje bij geldzaken.",
+        "calendar": [
+            {
+                "day": "monday",
+                "start": "14:00",
+                "end": "16:00",
+                "frequency": "even_weeks"
+            },
+            {
+                "day": "wednesday",
+                "start": "10:00",
+                "end": "12:00",
+                "frequency": "odd_weeks"
+            }
+        ],
+        "calendar_summary": "Even weken op maandag van 14:00 tot 16:00 uur, oneven weken op woensdag van 10:00 tot 12:00 uur."
+    },
+    {
+        "name": "Inloopspreekuur FNV",
+        "logo": {
+            "filename": "logo_fnv.png",
+            "alt": "FNV"
+        },
+        "description": "Voor vragen over je loopbaan of voor een gesprek over je werk met een coach.",
+        "calendar": [
+            {
+                "day": "wednesday",
+                "start": "10:00",
+                "end": "12:00",
+                "frequency": "even_weeks"
+            }
+        ],
+        "calendar_summary": "Even weken op woensdag van 10:00 tot 12:00 uur."
+    },
+    {
+        "name": "Inloopspreekuur VKB",
+        "logo": {
+            "filename": "Logo_vkb.png",
+            "alt": "VKB"
+        },
+        "description": "De Volkskredietbank Noord Oost Groningen biedt hulp bij schulden en financiële vragen namens en in de gemeenten Veendam, Eemsdelta, Het Hogeland, Oldambt of Pekela.",
+        "calendar": [
+            {
+                "day": "monday",
+                "start": "13:00",
+                "end": "15:00",
+                "frequency": "weekly"
+            }
+        ],
+        "calendar_summary": "Wekelijks op maandag van 13:00 tot 15:00 uur."
+    },
+    {
+        "name": "Vrijwilligers informatiepunt",
+        "logo": {
+            "filename": "logo_vip.png",
+            "alt": "Vrijwilligers Informatiepunt"
+        },
+        "description": "Kom langs bij ons voor het zoeken of plaatsen van vacatures, voor informatie, advies en belangenbehartiging en deskundigheidsbevordering van vrijwilligers.",
+        "calendar": [
+            {
+                "day": "monday",
+                "start": "14:00",
+                "end": "16:00",
+                "frequency": "first_of_month"
+            },
+            {
+                "day": "monday",
+                "start": "14:00",
+                "end": "16:00",
+                "frequency": "third_of_month"
+            }
+        ],
+        "calendar_summary": "Iedere eerste en derde maandag van de maand van 13:00 tot 15:00 uur."
+    },
+    {
+        "name": "Loket Stichting Leergeld",
+        "logo": {
+            "filename": "logo_stleergeld.png",
+            "alt": "Stichting Leergeld"
+        },
+        "description": "Je kunt een vergoeding aanvragen voor activiteiten op het gebied van onderwijs, sport, cultuur en welzijn.",
+        "calendar": [
+            {
+                "day": "monday",
+                "start": "14:00",
+                "end": "16:00",
+                "frequency": "first_of_month"
+            },
+            {
+                "day": "monday",
+                "start": "14:00",
+                "end": "16:00",
+                "frequency": "third_of_month"
+            }
+        ],
+        "calendar_summary": "Iedere eerste en derde maandag van de maand van 13:00 tot 15:00 uur."
+    },
     {
         "name": "Jongerenwerk deBasis",
         "logo": {
