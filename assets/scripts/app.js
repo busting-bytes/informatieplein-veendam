b56d23fd6145ac00feecdd8101e0b1fc76158732
KeizerGR <KeizerGR@proton.me>
2025-10-29 13:06:49 +0100

    openstelling kerstvakantie

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -9,6 +9,22 @@
  */
 const participants = [
     {
+        "name": "Openstelling Informatieplein",
+        "logo": {
+            "filename": "logo_bibliotheekveendam.png",
+            "alt": "Informatieplein"
+                },
+        "description": " ",
+        "calendar": [
+            {
+                "day": "monday",
+                "start": "14:00",
+                "end": "16:00",
+                "frequency": "weekly"
+                 ],
+        "calendar_summary": "Tijdens de kerstvakantie van eind december 2025 is het informatieplein gesloten van 20 december tot en met 2 januari."
+    },
+    {
         "name": "Hulp bij formulieren",
         "logo": {
             "filename": "logo_humanitas.png",
